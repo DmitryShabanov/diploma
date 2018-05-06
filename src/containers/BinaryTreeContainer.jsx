d7cb96d0b3b68eec27c9591198a6f2270a4a0bd0
Dmitry Shabanov <12rauon@gmail.com>
2018-05-06 14:14:14 +0300

    added lib

--- src/containers/BinaryTreeContainer.jsx
+++ src/containers/BinaryTreeContainer.jsx
@@ -4,29 +4,51 @@ import { Helmet } from 'react-helmet';
 import Graph from '../components/Graph';
 
 import { nodesFavoriteColor } from '../constants/';
+// import bintree from '../libs/bintree';
 
 class BinaryTreeContainer extends Component {
   state = {
-    graph: {
-      nodes: [
-        { id: 1, label: 'Node 1', color: nodesFavoriteColor },
-        { id: 2, label: 'Node 2', color: nodesFavoriteColor },
-        { id: 3, label: 'Node 3' },
-        { id: 4, label: 'Node 4', color: nodesFavoriteColor },
-        { id: 5, label: 'Node 5' },
-        { id: 6, label: 'Node 6' },
-      ],
-      edges: [
-        { from: 1, to: 2 },
-        { from: 1, to: 3 },
-        { from: 2, to: 4 },
-        { from: 2, to: 5 },
-        { from: 3, to: 6 },
-      ],
-    },
+    step: 0,
+    history: [
+      {
+        nodes: [
+          { id: 1, label: 'Node 1', color: nodesFavoriteColor },
+          { id: 2, label: 'Node 2', color: nodesFavoriteColor },
+          { id: 3, label: 'Node 3' },
+          { id: 4, label: 'Node 4', color: nodesFavoriteColor },
+          { id: 5, label: 'Node 5' },
+          { id: 6, label: 'Node 6' },
+        ],
+        edges: [
+          { from: 1, to: 2 },
+          { from: 1, to: 3 },
+          { from: 2, to: 4 },
+          { from: 2, to: 5 },
+          { from: 3, to: 6 },
+        ],
+      },
+    ],
   }
 
+  componentDidMount() {
+    // this.initializeTree();
+  }
+
+  // initializeTree = () => {
+  //   this.setState({
+  //     tree: bintree(this.state.mementoHistory),
+  //   });
+  // }
+
+  // nextStep = () => {
+  //   this.setState({ step: this.step + 1 });
+  // }
+
+  // prevStep = () => {}
+
   render() {
+    const { history, step } = this.state;
+
     return (
       <section>
         <Helmet>
@@ -34,7 +56,7 @@ class BinaryTreeContainer extends Component {
         </Helmet>
 
         <Graph
-          graph={this.state.graph}
+          graph={history[step]}
         />
       </section>
     );
